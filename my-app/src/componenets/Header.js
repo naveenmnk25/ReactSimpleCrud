b6673c73ba8b7f3da8c7dfe54719b8935e5a9d79
naveenmnk25 <97008354+naveenmnk25@users.noreply.github.com>
2023-03-10 16:12:54 +0530

    initial push

---
+++ my-app/src/componenets/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Heater = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Heater;
